ad59fd4df56d1e2fac7dc0b8fc828bf53082cf75
jdk8467 <jdk8467@naver.com>
2023-05-25 05:35:20 +0900

    feat: ArticleSection추가

---
+++ src/components/main/include/ArticleSection.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="article-list">
+    <div v-for="article in displayedArticles" :key="article.id" class="article-card" @click="goToArticleDetail(article.articleNo)">
+      <div class="article-photo-container">
+        <img :src="getImagePath(article.imgName)" alt="Article Photo" class="article-photo" />
+      </div>
+      <div class="article-content-container">
+        <div class="article-content">{{ article.content }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: 'ArticleComponent',
+  data() {
+    return {
+      articles: [],
+    };
+  },
+  created() {
+    let mytoken = localStorage.getItem("access-token");
+
+    axios({
+      headers: {"access-token": mytoken},
+      method: 'get',
+      url: `http://localhost:9999/api-article/articles`,
+      responseType: 'json'
+    }).then((response) => {
+      console.log(response.data);
+      this.articles = response.data
+    });
+  },
+  computed: {
+    displayedArticles() {
+      return this.articles.slice(0, 8);
+    },
+  },
+  methods:{
+    getImagePath(fileName) {
+      return require(`@/assets/${fileName}`);
+    },
+    goToArticleDetail(articleNo) {
+      this.$router.push({ path: `/article/${articleNo}` });
+    },
+  }
+};
+</script>
+
+<style scoped>
+.article-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+
+.article-card {
+  width: calc(25% - 20px);
+  background-color: #f5f5f5;
+  padding: 20px;
+  border-radius: 8px;
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  cursor: pointer;
+}
+
+.article-photo-container {
+  width: 100%;
+  height: 200px;
+  border-radius: 8px;
+  overflow: hidden;
+  margin-bottom: 20px;
+}
+
+.article-photo {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.article-content-container {
+  flex: 1;
+  display: flex;
+  align-items: center;
+}
+
+.article-content {
+  font-size: 16px;
+  line-height: 1.5;
+  text-align: center;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+</style>
--- src/components/main/include/SpecialSection.vue
+++ src/components/main/include/SpecialSection.vue
@@ -1,16 +1,22 @@
 <template>
-  <v-card class="special-component">
-    <v-card-text>
-      <div class="title">제목입니다!@!@!</div>
-      <div class="subtitle">로그인 후 사용해보세요!</div>
-      <div class="text">나만의 운동 루틴을 만들고 매일 인증해보세요!!</div>
-    </v-card-text>
-  </v-card>
+  <div class="special-component">
+    <div v-if="isLoggedIn" class="title">제목입니다!@!@!</div>
+    <div v-else class="title">Let's Start from here</div>
+    <div v-if="isLoggedIn" class="subtitle">로그인 후 사용해보세요!</div>
+    <div v-else class="subtitle">봉무게를 조상님이 들어준다</div>
+    <div v-if="isLoggedIn" class="text">나만의 운동 루틴을 만들고 매일 인증해보세요!!</div>
+    <div v-else class="text">다른 내용입니다!</div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'SpecialComponent',
+  data() {
+    return {
+      isLoggedIn: false, // 로그인 상태에 따라 변경될 데이터
+    };
+  },
 };
 </script>
 
@@ -23,22 +29,23 @@ export default {
 }
 
 .title {
+  margin-top: 30px;
   color: white;
-  font-size: 20px;
+  font-size: 60px !important; /* 폰트 크기에 !important 추가 */
   font-weight: bold;
   user-select: text;
 }
 
 .subtitle {
   color: #B2B2B2;
-  font-size: 20px;
-  margin-top: 10px;
+  font-size: 80px !important; /* 폰트 크기에 !important 추가 */
+  margin-top: 30px;
   user-select: text;
 }
 
 .text {
   color: #8C8C8C;
-  margin-top: 10px;
+  margin-top: 450px;
   font-size: 18px;
   user-select: text;
 }
